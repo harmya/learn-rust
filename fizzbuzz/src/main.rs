6ecca2d4e6a6c715e762bb70677f4308100a6d98
harmya <hvbhatt@purdue.edu>
2024-03-27 17:17:47 -0400

    ok i will do something normal now

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,7 +1,40 @@
 use std::io;
+use std::process;
 
 fn main() {
-    let mut input_number = String::new();
-    let arr = [1, 2, 4];
+    let mut input_n = String::new();
+    println!("Enter a number");
+    io::stdin()
+        .read_line(&mut input_n)
+        .expect("Give a number");
+
+    let input_n : usize = match input_n.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("bro, a number");
+            process::exit(1);
+        }
+    };    
+
+    let answer : Vec<String> = fizzbuzz(input_n);
     
+    println!("{:?}", answer);
+}
+
+fn fizzbuzz(n : usize) -> Vec<String> {
+
+    let mut vec : Vec<String> = Vec::with_capacity(n);
+    for i in 1..n+1 {
+        if i % 3 == 0 && i % 5 == 0 {
+            vec.push("FizzBuzz".to_string());
+        } else if i % 3 == 0 {
+            vec.push("Fizz".to_string());
+        } else if i % 5 == 0 {
+            vec.push("Buzz".to_string());
+        } else {
+            vec.push("".to_string());
+        }
+    }
+
+    return vec;
 }
